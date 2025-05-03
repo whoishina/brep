665de22a6df2e6249bb6163f46ddfd99470032dc
Ngo Hai <webhaidang@gmail.com>
2025-05-03 11:40:49 +0700

    Remove ChatPage component and related routing from the application, cleaning up unused files and simplifying the router configuration.

--- src/frontend/router.tsx
+++ src/frontend/router.tsx
@@ -4,7 +4,6 @@ import MainLayout from "./layouts/main/main";
 import HomePage from "./pages/home/home";
 import NotFoundPage from "./pages/notFound";
 import ErrorPage from "./pages/error";
-import ChatPage from "./pages/chat";
 
 export const reactRouter: RouteObject[] = [
   {
@@ -14,10 +13,6 @@ export const reactRouter: RouteObject[] = [
       {
         path: routes.home.root,
         element: <HomePage />,
-      },
-      {
-        path: routes.chat,
-        element: <ChatPage />,
       },
     ],
   },
